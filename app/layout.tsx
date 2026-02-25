74c9655aec5f422a44eee53cfdb56926c8bc6300
Hemant Nimble <nimblehemant@gmail.com>
2026-02-25 15:32:13 +0530

    vercel fix 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import Navbar from "@/components/Navbar";
-import { CartProvider } from "@/components/CartContext";
 import BottomNavbar from "@/components/BottomNavbar";
 import StoreProvider from "@/app/StoreProvider";
 
